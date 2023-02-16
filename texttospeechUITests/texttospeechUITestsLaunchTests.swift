c1a49aa2af399a20038fc9521dbac0a56ec7fc61
maha <mealmdhebri22@twq.idserve.net>
2023-02-16 15:46:03 +0300

    Initial Commit

---
+++ texttospeechUITests/texttospeechUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  texttospeechUITestsLaunchTests.swift
+//  texttospeechUITests
+//
+//  Created by maha mdhebri on 25/07/1444 AH.
+//
+
+import XCTest
+
+final class texttospeechUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
